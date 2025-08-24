5237dc8f1cc8ecd8dbe16b8315edb76d2463cb73
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-08-24 14:09:00 -0600

    Fase2

--- Uber/Fase2.py
+++ Uber/Fase2.py
@@ -1,1 +1,8 @@
-## Aquí va el code we
+import pandas as pd
+## In this project we will
+## learn how to connect an Excel 
+## database to python to solve a common issue for 
+## small or big companies.
+
+df = pd.read_excel("uber.xlsx") ## Coonect the Excel file to python
+print(df) ##Print the data frame
